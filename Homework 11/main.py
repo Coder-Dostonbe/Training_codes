8cba2fa0bc6df3f68fcb5852d9b07b4739c5c8f3
Serebro <yusupovdostonbek21@gmail.com>
2025-12-09 23:42:20 +0500

    test github Homework 11

---
+++ Homework 11/main.py
@@ -0,0 +1,37 @@
+"""Task 1"""
+# 1. Ikkita qiymat terilishini so’raydigan dastur yozing agar ulardan bittasi bo’lsa ham son bo’lmasa,
+# konkatenatsiya bajarilishi kerak.Aks holda sonlar qo’shilishi lozim.
+
+# a = input("Raqam kiriting: ")
+# b = input("Yana bitta raqam kiriting: ")
+#
+# try:
+#     c = int(a) + int(b)
+#     print(c)
+# except:
+#     d = a + b
+#     print(d)
+
+"""Task 2"""
+# 2. Pastda keltirilgan  kod food ro’yxatdagi so’zlarning 5-chi harfini fifth ro’yxat ichiga joylashtiradi.
+# Ammo hozir kod noto’g’ri ishlamoqda va hato chiqarmoqda.try/except jumlani joylashtiring u tufayli kod ishga
+# tushadi va ro’yxat yaratiladi.Agar so’z yetarlicha uzun bo’lmasa u hech narsa qaytarishi kerak emas.Eslatma.
+# pass - operatori nol operatsiyasi hisoblanadi.hech narsa sodir bo’lmaydi kod ishga tushirilganda.
+#
+# food = ["chocolate", "chicken", "corn", "sandwich", "soup", "potatoes", "beef", "lox", “lemonade"]
+#
+# fifth = []
+# for x in food:
+#    fifth.append(x[4])
+
+# food = ["chocolate", "chicken", "corn", "sandwich", "soup", "potatoes", "beef", "lox", "lemonade"]
+#
+# fifth = []
+# for x in food:
+#     try:
+#         fifth.append(x[3])
+#     except:
+#         pass
+# print(fifth)
+
+
