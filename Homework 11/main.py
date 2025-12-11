93b42398b7be51f60e476f55a2f12a6cfde07547
Coder-Dostonbe <yusupovdostonbek21@gmail.com>
2025-12-11 19:23:07 +0500

    Fix input handling and add error handling for list

--- Homework 11/main.py
+++ Homework 11/main.py
@@ -2,15 +2,15 @@
 # 1. Ikkita qiymat terilishini so’raydigan dastur yozing agar ulardan bittasi bo’lsa ham son bo’lmasa,
 # konkatenatsiya bajarilishi kerak.Aks holda sonlar qo’shilishi lozim.
 
-# a = input("Raqam kiriting: ")
-# b = input("Yana bitta raqam kiriting: ")
-#
-# try:
-#     c = int(a) + int(b)
-#     print(c)
-# except:
-#     d = a + b
-#     print(d)
+a = input("Raqam kiriting: ")
+b = input("Yana bitta raqam kiriting: ")
+
+try:
+    c = int(a) + int(b)
+    print(c)
+except:
+    d = a + b
+    print(d)
 
 """Task 2"""
 # 2. Pastda keltirilgan  kod food ro’yxatdagi so’zlarning 5-chi harfini fifth ro’yxat ichiga joylashtiradi.
@@ -19,19 +19,19 @@
 # pass - operatori nol operatsiyasi hisoblanadi.hech narsa sodir bo’lmaydi kod ishga tushirilganda.
 #
 # food = ["chocolate", "chicken", "corn", "sandwich", "soup", "potatoes", "beef", "lox", “lemonade"]
-#
+
 # fifth = []
 # for x in food:
 #    fifth.append(x[4])
 
-# food = ["chocolate", "chicken", "corn", "sandwich", "soup", "potatoes", "beef", "lox", "lemonade"]
-#
-# fifth = []
-# for x in food:
-#     try:
-#         fifth.append(x[3])
-#     except:
-#         pass
-# print(fifth)
+food = ["chocolate", "chicken", "corn", "sandwich", "soup", "potatoes", "beef", "lox", "lemonade"]
+
+fifth = []
+for x in food:
+    try:
+        fifth.append(x[3])
+    except:
+        pass
+print(fifth)
 
 
